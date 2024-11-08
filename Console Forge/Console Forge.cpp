2b4295f7cb6220af8531e07ce6a70e2a43ba5653
Bogdan <bogdan1000leitan2007@gmail.com>
2024-11-09 00:07:15 +0200

    update 'info' section and 'start text'

--- Console Forge/Console Forge.cpp
+++ Console Forge/Console Forge.cpp
@@ -6,8 +6,8 @@ int main() {
     SetConsoleTitle(L"Console Forge");
     std::string command;
 
-    std::cout << "Console Forge web controller\n";
-    std::cout << "Open source -> https://github.com/Bogdan-Cpp/ConsoleForge\n";
+    std::cout << "Console Forge 'web controller'\n";
+    std::cout << "-> Write 'info' for more commands \n";
     std::cout << "\n";
 
     do {
@@ -15,9 +15,12 @@ int main() {
         std::cout << "web/command/-> ";
         std::cin >> command;
         if (command == "info") {
+            std::cout << std::endl;
             std::cout << "yt -> YouTube\n";
             std::cout << "ai -> ChatGpt\n";
-            std::cout << "mail -> Google Gmail inbox";
+            std::cout << "mail -> Google Gmail inbox \n";
+            std::cout << "ws -> Whatsapp \n";
+            std::cout << "gh -> Github \n";
         }
 
         else if (command == "yt") { ShellExecute(0, L"open", L"https://www.youtube.com", NULL, NULL, SW_SHOW); }
@@ -26,7 +29,7 @@ int main() {
 
         else if (command == "mail") { ShellExecute(0, L"open", L"https://mail.google.com/mail/u/0/#inbox", NULL, NULL, SW_SHOW); }
 
-        else if (command == "whatsapp") { ShellExecute(0, L"open", L"https://web.whatsapp.com/", NULL, NULL, SW_SHOW); }
+        else if (command == "ws") { ShellExecute(0, L"open", L"https://web.whatsapp.com/", NULL, NULL, SW_SHOW); }
 
         else if (command == "gh") { ShellExecute(0, L"open", L"https://github.com/", NULL, NULL, SW_SHOW); }
 
@@ -35,4 +38,5 @@ int main() {
 
     system("pause");
     return 0;
+
 }
