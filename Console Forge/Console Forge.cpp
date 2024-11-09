4761ee6947621fcb833b9879f3f079002cbdde59
Bogdan <bogdan1000leitan2007@gmail.com>
2024-11-09 10:25:17 +0200

    google seartch and classes

--- Console Forge/Console Forge.cpp
+++ Console Forge/Console Forge.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <windows.h>
 
+class Google {
+    public:
+        std::string seartch;
+
+        void Seartch() {
+            std::cout << "Seartch on Google-> ";
+            std::cin >> seartch;
+
+            std::wstring url = L"https://www.google.com/search?q=";
+            url += std::wstring(seartch.begin(), seartch.end());
+
+            ShellExecute(0, L"open", url.c_str(), NULL, NULL, SW_SHOW);
+        }
+};
 
 int main() {
     SetConsoleTitle(L"Console Forge");
@@ -21,6 +35,7 @@ int main() {
             std::cout << "mail -> Google Gmail inbox \n";
             std::cout << "ws -> Whatsapp \n";
             std::cout << "gh -> Github \n";
+            std::cout << "sr -> Seartch on Google \n";
         }
 
         else if (command == "yt") { ShellExecute(0, L"open", L"https://www.youtube.com", NULL, NULL, SW_SHOW); }
@@ -32,6 +47,11 @@ int main() {
         else if (command == "ws") { ShellExecute(0, L"open", L"https://web.whatsapp.com/", NULL, NULL, SW_SHOW); }
 
         else if (command == "gh") { ShellExecute(0, L"open", L"https://github.com/", NULL, NULL, SW_SHOW); }
+
+        else if (command == "sr") {
+            Google srch;
+            srch.Seartch();
+        }
 
         std::cout << std::endl;
     } while (command != "exit");
